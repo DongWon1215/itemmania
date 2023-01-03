e65a92aeed30568399933459ee0505e1cf931f0b
dwkang <ejddjsl123@naver.com>
2023-01-03 14:47:30 +0900

    Create Project

---
+++ src/main/java/com/itemmania/ItemmaniaApplication.java
@@ -0,0 +1,16 @@
+package com.itemmania;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log4j2
+public class ItemmaniaApplication {
+
+    public static void main(String[] args) {
+        log.info("테스트용 문자");
+        SpringApplication.run(ItemmaniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itemmania/ItemmaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itemmania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemmaniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
