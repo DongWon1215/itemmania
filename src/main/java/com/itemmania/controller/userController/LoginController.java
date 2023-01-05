72c94a8134306564202466d6369dc68ec2485b1d
dwkang <ejddjsl123@naver.com>
2023-01-05 15:58:07 +0900

    patch val name

--- src/main/java/com/itemmania/controller/userController/LoginController.java
+++ src/main/java/com/itemmania/controller/userController/LoginController.java
@@ -1,6 +1,8 @@
 package com.itemmania.controller.userController;
 
 import com.itemmania.entity.UserEntity;
+import com.itemmania.service.userService.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,15 +12,18 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public String login(HttpServletRequest request)
     {
-        UserEntity user =;
+//        UserEntity user = userService.getUser();
 
 
         HttpSession session = request.getSession();
 
-        session.setAttribute("",);
+//        session.setAttribute("userInfo",user);
 
         return "/";
     }
--- src/main/java/com/itemmania/service/userService/UserService.java
+++ src/main/java/com/itemmania/service/userService/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class userService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
