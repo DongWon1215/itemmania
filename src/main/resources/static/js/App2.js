3761475e77b1f7ce92a73e2905998c67591aef53
김주환 <juhwan71047@gmail.com>
2023-01-09 16:24:41 +0900

    ReactDOM

---
+++ src/main/resources/static/js/App2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App2(props) {
+    return (
+        <div>김주환</div>,
+            console.log("김주환ㄴㄴ")
+
+    )
+        ;
+}
+
+export default App2;
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App2 from './App2';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App2/>
+    </React.StrictMode>,
+    document.querySelector('#root'));
+}
+
+export default Index;
