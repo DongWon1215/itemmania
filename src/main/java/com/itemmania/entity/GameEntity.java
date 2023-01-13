f2321fc6f1f83ec76962c9b38c12ab07d74c8bec
dabinHam <113006964+dabinHam@users.noreply.github.com>
2023-01-13 11:11:18 +0900

    game entity 추가

---
+++ src/main/java/com/itemmania/entity/GameEntity.java
@@ -0,0 +1,29 @@
+package com.itemmania.entity;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "game")
+public class GameEntity {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int gameNum;
+
+    @Column(unique = true)
+    @NotNull
+    @JoinColumn(name = "gameNum")
+    private String gameName;
+
+
+}
