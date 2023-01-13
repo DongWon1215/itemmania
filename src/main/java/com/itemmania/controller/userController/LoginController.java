72e2fc11290b8aa58ed903da1a4ed80826e26def
김주환 <juhwan71047@gmail.com>
2023-01-13 11:15:14 +0900

    Revert "Merge branch 'master' into boardList2_ju"
    This reverts commit 0d18157a84c22e9a7f56ac17819894dde2e21618, reversing
    changes made to 1b2d26b88b739a161d3d6b8b25eb0a6343b5d94c.

--- src/main/java/com/itemmania/controller/userController/LoginController.java
+++ src/main/java/com/itemmania/controller/userController/LoginController.java
@@ -1,48 +1,37 @@
 package com.itemmania.controller.userController;
 
-import com.itemmania.domain.LoginRequest;
-import com.itemmania.entity.UserEntity;
 import com.itemmania.service.userService.UserService;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/login")
-@Log4j2
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public String loginPage()
+    public String logiPage()
     {
         return "userForm/loginForm";
     }
 
     @PostMapping
-    public String login(HttpServletRequest request, @RequestBody LoginRequest loginRequest)
+    public String login(HttpServletRequest request)
     {
-        log.info("일단 들어오긴 했다");
-
-        log.info("getUserName = " + loginRequest.getUser_name());
-        log.info("getUserPassword = " + loginRequest.getUser_password());
+//        UserEntity user = userService.getUser();
 
 
-        if(!userService.isExistUser(loginRequest.getUser_name(), loginRequest.getUser_password()))
-            return "/UserForm/registerForm";
+//        HttpSession session = request.getSession();
 
-        UserEntity user = userService.getUser(loginRequest.getUser_name(), loginRequest.getUser_password());
+//        session.setAttribute("userInfo",user);
 
-        HttpSession session = request.getSession();
-
-        session.setAttribute("userInfo",user);
-
-        return "index";
+        return "/";
     }
 }
