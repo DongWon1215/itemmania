ef8c8addbb09df70bc0e1ed121925483febc028e
김주환 <juhwan71047@gmail.com>
2023-01-13 11:27:49 +0900

    Revert "Revert "Merge branch 'master' into boardList2_ju""
    This reverts commit 72e2fc11290b8aa58ed903da1a4ed80826e26def.

--- src/main/java/com/itemmania/controller/userController/LoginController.java
+++ src/main/java/com/itemmania/controller/userController/LoginController.java
@@ -1,37 +1,48 @@
 package com.itemmania.controller.userController;
 
+import com.itemmania.domain.LoginRequest;
+import com.itemmania.entity.UserEntity;
 import com.itemmania.service.userService.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/login")
+@Log4j2
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public String logiPage()
+    public String loginPage()
     {
         return "userForm/loginForm";
     }
 
     @PostMapping
-    public String login(HttpServletRequest request)
+    public String login(HttpServletRequest request, @RequestBody LoginRequest loginRequest)
     {
-//        UserEntity user = userService.getUser();
+        log.info("일단 들어오긴 했다");
+
+        log.info("getUserName = " + loginRequest.getUser_name());
+        log.info("getUserPassword = " + loginRequest.getUser_password());
 
 
-//        HttpSession session = request.getSession();
+        if(!userService.isExistUser(loginRequest.getUser_name(), loginRequest.getUser_password()))
+            return "/UserForm/registerForm";
 
-//        session.setAttribute("userInfo",user);
+        UserEntity user = userService.getUser(loginRequest.getUser_name(), loginRequest.getUser_password());
 
-        return "/";
+        HttpSession session = request.getSession();
+
+        session.setAttribute("userInfo",user);
+
+        return "index";
     }
 }
---
+++ src/main/java/com/itemmania/domain/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.itemmania.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class LoginRequest {
+
+    private String user_name;
+
+    private String user_password;
+}
