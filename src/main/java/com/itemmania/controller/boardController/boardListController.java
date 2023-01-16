5a09a5bc6d12a8d011e9b7a11c8dbd4a8233964b
김주환 <112999677+kimjuhwan7@users.noreply.github.com>
2023-01-16 23:01:54 +0900

    json콘솔창 출력

--- src/main/java/com/itemmania/controller/boardController/boardListController.java
+++ src/main/java/com/itemmania/controller/boardController/boardListController.java
@@ -4,6 +4,7 @@ import com.itemmania.entity.BoardEntity;
 import com.itemmania.service.boardService.BoardListService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,17 +12,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Log4j2
-@Controller
+@RestController
 public class boardListController {
     @Autowired
     private BoardListService boardListService;
 
     @GetMapping("/board/boardList/{DealCheck}")
-    public String getsaleList(Model model, @PathVariable("DealCheck") String DealCheck) {
+    public List<BoardEntity> getsaleList(Model model, @PathVariable("DealCheck") String DealCheck) {
+        log.info("보드리스트 컨트롤러 들어옴보드리스트 컨트롤러 들어옴");
         // List<BoardEntity> getsaleList = boardListService.getDealCheckList(DealCheck);
         //model.addAttribute("boardList", getsaleList);
         List<BoardEntity> dealCheckList = boardListService.getDealCheckList(DealCheck);
         model.addAttribute("boarList", dealCheckList);
-        return "/board/boardList";
+        return dealCheckList;
     }
 }
