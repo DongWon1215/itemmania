c9a88704757c77da888799748e13768d130610d1
dwkang <ejddjsl123@naver.com>
2023-01-25 13:08:55 +0900

    regist member incomplete

--- src/main/java/com/itemmania/controller/userController/RegisterController.java
+++ src/main/java/com/itemmania/controller/userController/RegisterController.java
@@ -1,7 +1,11 @@
 package com.itemmania.controller.userController;
 
+import com.itemmania.domain.LoginRequest;
+import com.itemmania.domain.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +16,13 @@ public class RegisterController {
     public String registpage()
     {
         return "/UserForm/registerForm";
+    }
+
+    @PostMapping
+    public String registMember(@RequestBody UserDTO userDTO)
+    {
+
+
+        return "/index";
     }
 }
