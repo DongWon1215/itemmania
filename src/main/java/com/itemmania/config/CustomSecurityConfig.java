c87072d73417d61c6228d3607fe2c1888514ec9b
dwkang <ejddjsl123@naver.com>
2023-01-27 17:02:17 +0900

    refactoring security logic and shortcut ui

--- src/main/java/com/itemmania/config/CustomSecurityConfig.java
+++ src/main/java/com/itemmania/config/CustomSecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,13 +14,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class CustomSecurityConfig {
 
     @Bean
-public SecurityFilterChain filterChai(HttpSecurity httpSecurity)throws Exception
+    public SecurityFilterChain filterChai(HttpSecurity httpSecurity)throws Exception
     {
         httpSecurity.csrf().disable();
 
-        httpSecurity.authorizeHttpRequests().antMatchers("/").permitAll()
+        httpSecurity.authorizeHttpRequests()
                 .antMatchers("/board/**").hasRole("USER")
-                .antMatchers("/myroom/**").hasRole("USER");
+                .antMatchers("/myroom/**").hasRole("USER")
+                .anyRequest().permitAll();
 
         httpSecurity.formLogin().loginPage("/login");
 
@@ -27,5 +30,11 @@ public SecurityFilterChain filterChai(HttpSecurity httpSecurity)throws Exception
         httpSecurity.rememberMe().key("123456789").rememberMeParameter("remember-me").tokenValiditySeconds(60*60*10);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
