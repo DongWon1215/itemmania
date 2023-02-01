2327a39bc7ab19429958582c46ac354ff2064bf7
jennakim1007 <juhwan71047@gmail.com>
2023-02-01 16:55:33 +0900

    메인검색리스트 출력완료

--- src/main/java/com/itemmania/controller/boardController/boardList/BoardListController.java
+++ src/main/java/com/itemmania/controller/boardController/boardList/BoardListController.java
@@ -3,6 +3,9 @@ package com.itemmania.controller.boardController.boardList;
 
 import com.itemmania.domain.BoardSearchOption;
 import com.itemmania.domain.BoardSearchOptionVO;
+import com.itemmania.entity.GameEntity;
+import com.itemmania.entity.GameServerEntity;
+import com.itemmania.service.boardService.Search.BoardGameSearchService;
 import com.itemmania.service.boardService.Search.BoardSearchService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Controller
@@ -18,6 +23,10 @@ public class BoardListController {
 
     @Autowired
     private BoardSearchService boardSearchService;
+
+
+    @Autowired
+    private BoardGameSearchService getGameServerService;
 
     @PostMapping("/board/boardList")
     public String subsearch(Model model, BoardSearchOption boardSearchOption) {
@@ -29,10 +38,23 @@ public class BoardListController {
 
         List<BoardSearchOptionVO> p_searchDataCheck = boardSearchService.getP_SearchDataCheck(boardSearchOption);
         List<BoardSearchOptionVO> searchDataCheck = boardSearchService.getSearchDataCheck(boardSearchOption);
+
+
+        List<GameServerEntity> serverNameList = getGameServerService.getServerService();
+
+        List<GameEntity> gameNameList = getGameServerService.getGameService();
+
         log.info("서브서치 컨트롤러 리스트 " + searchDataCheck);
+        log.info("게임서버검색" + gameNameList);
         //프리미엄, 일반 리스트
         model.addAttribute("boarPList", p_searchDataCheck);
         model.addAttribute("boarList", searchDataCheck);
+
+        // 전체 게임리스트
+        model.addAttribute("searchGame", gameNameList);
+
+        // 전체 서버리스트
+        model.addAttribute("searchServer", serverNameList);
 
 
         // 사용자가 검색한 게임서버 value
