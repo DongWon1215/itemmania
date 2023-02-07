e08530d701cc20d7973e98a55ac51d92454f2644
김주환 <juhwan71047@gmail.com>
2023-02-07 18:36:27 +0900

    trade입력기능 구현중

--- src/main/java/com/itemmania/controller/boardController/requestView/BoardRequestController.java
+++ src/main/java/com/itemmania/controller/boardController/requestView/BoardRequestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -42,4 +43,23 @@ public class BoardRequestController {
 
     }
 
+    @PostMapping("/board/requestPage")
+    public void postBoardRequest(@RequestParam(value = "boardNum") int boardNum, @RequestParam(value = "deal") String dealCheck) {
+        log.info("파라미터 확인 boardNum" + boardNum + "deal" + dealCheck);
+        log.info("이이이이이이이이이");
+
+        log.info("거래번호" +
+                "구매 마일리지 내역" +
+                "판매 마일리지 내역" +
+                "거래 게시글 번호" +
+                "거래 금액 (거래 비정상 종료시 0)" +
+                "거래 수량" +
+                "정상 종료 구분 (0:비정상 1:정상)" +
+                "거래 종료 시간");
+
+
+
+
+
+    }
 }
