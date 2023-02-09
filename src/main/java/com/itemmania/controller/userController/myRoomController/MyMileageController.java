f283bc64d8f26154147c0a492aca61adcd6dd4f3
jennakim1007 <jennakim1007@gmail.com>
2023-02-09 11:07:08 +0900

    마일리지 표기 변경

--- src/main/java/com/itemmania/controller/userController/myRoomController/MyMileageController.java
+++ src/main/java/com/itemmania/controller/userController/myRoomController/MyMileageController.java
@@ -1,16 +1,27 @@
 package com.itemmania.controller.userController.myRoomController;
 
+import com.itemmania.entity.UserEntity;
+import com.itemmania.service.mileageService.MileageViewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/myroom/mymileage")
 public class MyMileageController {
 
+    @Autowired
+    private MileageViewService mileageViewService;
+
     @GetMapping
-    public String getMypageForm()
+    public String getMypageForm(HttpSession session, Model model)
     {
+        UserEntity user = (UserEntity) session.getAttribute("userInfo");
+        model.addAttribute("mileage", mileageViewService.getUserMileage(user.getUserNum()));
         return "userForm/myRoom/myMileage";
     }
 
