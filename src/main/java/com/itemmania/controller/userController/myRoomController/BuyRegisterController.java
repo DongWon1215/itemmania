b377c26af816fa2a5915582036de0459d1948e17
김주환 <juhwan71047@gmail.com>
2023-02-14 20:41:57 +0900

    null값 출력

--- src/main/java/com/itemmania/controller/userController/myRoomController/BuyRegisterController.java
+++ src/main/java/com/itemmania/controller/userController/myRoomController/BuyRegisterController.java
@@ -1,20 +1,34 @@
 package com.itemmania.controller.userController.myRoomController;
 
+import com.itemmania.entity.TradeEntity;
+import com.itemmania.service.tradeService.detailsTrade.TradeSellerService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
+@Log4j2
 @Controller
 @RequestMapping("/myroom/buyRegister")
 public class BuyRegisterController {
 
+
+    @Autowired
+    private TradeSellerService tradeSellerService;
+
     @GetMapping
-    public String getMypageForm(Module module, Principal principal) {
+    public String getMypageForm(Model model, Principal principal) {
 //        log.info("Principal?" + principal.getName());
 
+        List<TradeEntity> tradeSeller = tradeSellerService.getTradeSeller(principal.getName());
+        log.info("tradeSeller?" + tradeSeller);
 
+        model.addAttribute("tradeSeller", tradeSeller);
 
 
 
