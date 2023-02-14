9886c79bbf70d42dc84c531c5f8e5205a9e24033
김주환 <juhwan71047@gmail.com>
2023-02-14 20:22:06 +0900

    마이룸 판매관련페이지 출력오류수정

---
+++ src/test/java/com/itemmania/trade/유저번호로_거래테이블get.java
@@ -0,0 +1,21 @@
+package com.itemmania.trade;
+
+import com.itemmania.service.tradeService.detailsTrade.TradeSellerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class 유저번호로_거래테이블get {
+
+
+    @Autowired
+    private TradeSellerService tradeSellerService;
+
+
+    @Test
+    public void get테이블(){
+        tradeSellerService.getTradeSeller("5");
+    }
+
+}
