cc801318a0a0bcc7dee20c9c9f48570dfb71a2e8
dwkang <ejddjsl123@naver.com>
2023-02-15 13:28:11 +0900

    Update BoardListReplaceController.java

--- src/main/java/com/itemmania/controller/boardController/boardList/BoardListReplaceController.java
+++ src/main/java/com/itemmania/controller/boardController/boardList/BoardListReplaceController.java
@@ -1,12 +1,19 @@
 package com.itemmania.controller.boardController.boardList;
 
+import com.itemmania.entity.MileageEntity;
 import com.itemmania.entity.TradeEntity;
+import com.itemmania.entity.UserEntity;
 import com.itemmania.repository.TradeRepository;
+import com.itemmania.service.mileageService.MileageInsertService;
 import com.itemmania.service.tradeService.BoardStateChangeService;
 import com.itemmania.service.tradeService.detailsTrade.TradeSellerService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 
 @Log4j2
 @RestController
@@ -22,8 +29,11 @@ public class BoardListReplaceController {
     @Autowired
     private TradeSellerService tradeSellerService;
 
+    @Autowired
+    private MileageInsertService mileageInsertService;
+
     @PostMapping("/myroom/replace")
-    public int setTradeDenai(@RequestBody String tradeNum)
+    public int setTradeDenai(HttpServletRequest request, @RequestBody String tradeNum)
     {
         tradeNum = tradeNum.substring(0, tradeNum.length() - 1);
 
@@ -32,6 +42,26 @@ public class BoardListReplaceController {
         tradeSellerService.setTradeDenai(tNum);
 
         TradeEntity trade = tradeRepository.findByTradeNum(tNum);
+
+        MileageEntity writerMileage = MileageEntity.builder().userNum(trade.getBoardNum().getUserNum())
+                .mileageTime(LocalDateTime.now())
+                .mileageType("구매취소")
+                .mileageDescription("#구매취소 : " + trade.getBoardNum().getBoardNum())
+                .mileageIn(0)
+                .mileageOut(trade.getTradeAmount()).build();
+
+        HttpSession session = request.getSession();
+        UserEntity user = (UserEntity) session.getAttribute("userInfo");
+
+        MileageEntity requesterMileage = MileageEntity.builder().userNum(user)
+                .mileageTime(LocalDateTime.now())
+                .mileageType("구매취소")
+                .mileageDescription("#구매취소 : " + trade.getBoardNum().getBoardNum())
+                .mileageIn(trade.getTradeAmount())
+                .mileageOut(0).build();
+
+        mileageInsertService.setMileage(writerMileage);
+        mileageInsertService.setMileage(requesterMileage);
 
         boardStateChangeService.boardTradeStateChange(0,trade.getBoardNum().getBoardNum());
 
